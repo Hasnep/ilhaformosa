8a7c4000f5462c734eb48f6950199cfb2cdb123b
Hannes <hasnep@gmail.com>
2018-07-18 13:11:36 +0800

    Make the title import only the floor function from math.

--- title.py
+++ title.py
@@ -1,13 +1,13 @@
 import os
 from pyfiglet import Figlet
-import math
+from math import floor
 
 
 def print_title():
     terminal_width = os.get_terminal_size()[0]
     title = Figlet(font='script').renderText('Ilha Formosa')
     title_width = len(title.split("\n", 1)[0])
-    n_blanks = math.floor((terminal_width - title_width)/2)
+    n_blanks = floor((terminal_width - title_width)/2)
     blanks = " " * n_blanks
     title = blanks + title
     title = title.replace("\n", "\n" + blanks)
