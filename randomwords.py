7d6ba3c532fd108c24ca91a2c6c2d6daa5b48b54
Hannes <hasnep@gmail.com>
2018-07-19 21:21:29 +0800

    Move text files to resources folder

--- randomwords.py
+++ randomwords.py
@@ -2,11 +2,11 @@ import os
 import random
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-nouns_path = os.path.join(THIS_FOLDER, 'nouns.txt')
+nouns_path = os.path.join(THIS_FOLDER, 'resources/nouns.txt')
 with open(nouns_path, "r") as myfile:
     nouns = myfile.read().splitlines()
 
-adjectives_path = os.path.join(THIS_FOLDER, 'adjectives.txt')
+adjectives_path = os.path.join(THIS_FOLDER, 'resources/adjectives.txt')
 with open(adjectives_path, "r") as myfile:
     adjectives = myfile.read().splitlines()
 
