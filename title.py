2a7a173850eb48d0d5f9554fc70c5bc93f94a295
Hannes <hasnep@gmail.com>
2018-07-19 10:33:37 +0800

    Make fancy title work in some situations it didn't before.

--- title.py
+++ title.py
@@ -2,20 +2,20 @@ import os
 from pyfiglet import Figlet
 from math import floor
 
-
 def title_print():
     """A function that prints the title in a fancy way."""
-    terminal_width = os.get_terminal_size()[0]
     title = Figlet(font='script').renderText('Ilha Formosa')
     title_width = len(title.split("\n", 1)[0])
-    n_blanks = floor((terminal_width - title_width)/2)
-    blanks = " " * n_blanks
-    title = blanks + title
-    title = title.replace("\n", "\n" + blanks)
-    title = title.replace(" " * title_width + "\n", "")
-    print("=" * terminal_width + title + "\n" + "=" * terminal_width)
+    try:
+        terminal_width = os.get_terminal_size()[0]
+        n_blanks = floor((terminal_width - title_width) / 2)
+        blanks = " " * n_blanks
+        title = blanks + title
+        title = title.replace("\n", "\n" + blanks)
+        title = title.replace(" " * title_width + "\n", "")
+        print("=" * terminal_width + title + "\n" + "=" * terminal_width)
+    except:
+        print("=" * title_width + "\n" + title + "=" * title_width)
 
-try:
-    title_print()
-except:
-    print("Ilha Formosa!")
+
+title_print()
