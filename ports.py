6f6d774bdbe25b3f245719404111d76a4199f4b0
Hannes <hasnep@gmail.com>
2018-07-19 10:55:12 +0800

    Add a visited and discovered flag to ports.

--- ports.py
+++ ports.py
@@ -1,13 +1,14 @@
 """Defining the ports classes"""
 from buildings import *
 
-# TODO: Add a flag to indicate if a port has been visited or discovered yet
 
 class Port(object):
     def __init__(self, name):
         self.name = name
         self.landing_message = "You land in %s" % name
         self.description = "%s is a port" % self.name
+        self.visited = False
+        self.discovered = False
         self.buildings = []
         for k in all_building_types:
             self.buildings.append(k(self.name))
@@ -17,11 +18,10 @@ all_ports = ["taipei", "shanghai", "tokyo"]
 
 # TODO: add distances
 # ports_distances = {}
-#
 # for city1, cords1 in cords.items():
 #     ports_distances[city1] = {}
 #     for city2, cords2 in cords.items():
 #         ports_distances[city1][city2] = dist(cords1, cords2)
 
 
-world = {k: Port(k.title()) for k in all_ports}
+world = {k: Port(k.title()) for k in all_ports}  # create port objects for all ports
