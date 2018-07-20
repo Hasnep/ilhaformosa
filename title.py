de9c46c3bef354ca641fca31215da33508ccdf4e
Hannes <hasnep@gmail.com>
2018-07-20 15:26:51 +0800

    Make title work on more platforms

--- title.py
+++ title.py
@@ -6,21 +6,25 @@ version_number = "v0.1.0"
 
 def title_print():
     """A function that prints the title in a fancy way."""
-    title = Figlet(font="script").renderText("Ilha Formosa")
-    title_width = len(title.split("\n", 1)[0])
     try:
-        terminal_width = os.get_terminal_size()[0]
-        n_blanks = floor((terminal_width - title_width) / 2)
-        blanks = " " * n_blanks
-        title = blanks + title
-        title = title.replace("\n", "\n" + blanks)
-        title = title.replace(" " * title_width + "\n", "")
-        print("=" * terminal_width + title + "\n" + "=" * terminal_width)
+        title = Figlet(font="script").renderText("Ilha Formosa")
+        title_width = len(title.split("\n", 1)[0])
+        try:
+            terminal_width = os.get_terminal_size()[0]
+            n_blanks = floor((terminal_width - title_width) / 2)
+            blanks = " " * n_blanks
+            title = blanks + title
+            title = title.replace("\n", "\n" + blanks)
+            title = title.replace(" " * title_width + "\n", "")
+            print("=" * terminal_width + title + "\n" + "=" * terminal_width)
+        except:
+            title = title.replace(" " * title_width + "\n", "")
+            title = title + " " * title_width + "\n"
+            print("=" * title_width + "\n" + title + "=" * title_width)
+        print(version_number)
     except:
-        title = title.replace(" " * title_width + "\n", "")
-        title = title + " " * title_width + "\n"
-        print("=" * title_width + "\n" + title + "=" * title_width)
+        print("Ilha Formosa! - " + version_number)
 
 
 title_print()
-print(version_number)
+
