3660429ebe28ad155a590b75defcae5573add40f
Hannes <hasnep@gmail.com>
2018-07-30 13:26:44 +0800

    Rewrite title

--- title.py
+++ title.py
@@ -1,33 +1,10 @@
-import os
-from pyfiglet import Figlet
-import math
-
-version_number = "v0.1.092"
-
-# TODO: Make some sort of system for integrating ascii art with text.
+version_number = "v0.1.093"
 
 
-def title_print():
-    """A function that prints the title in a fancy way."""
-    global version_number
-    try:
-        title = Figlet(font="script").renderText("Ilha Formosa")  # TODO: Make the title using an online generator and put into the script as a string.
-        title_width = len(title.split("\n", 1)[0])
-        try:
-            terminal_width = os.get_terminal_size()[0]
-            n_blanks = math.floor((terminal_width - title_width) / 2)
-            blanks = " " * n_blanks
-            title = blanks + title
-            title = title.replace("\n", "\n" + blanks)
-            title = title.replace(" " * title_width + "\n", "")
-            print("=" * terminal_width + title + "\n" + "=" * terminal_width)
-        except:
-            title = title.replace(" " * title_width + "\n", "")
-            title = title + " " * title_width + "\n"
-            print("=" * title_width + "\n" + title + "=" * title_width)
-        print(version_number)
-    except:
-        print("Ilha Formosa! - " + version_number)
+def title_print(ruler="~"):
+    title = " '||' '||  '||                '||''''| \n  ||   ||   || ..    ....      ||  .     ...   ... ..  .. .. ..     ...    ....   .... \n  ||   ||   ||' ||  '' .||     ||''|   .|  '|.  ||' ''  || || ||  .|  '|. ||. '  '' .|| \n  ||   ||   ||  ||  .|' ||     ||      ||   ||  ||      || || ||  ||   || . '|.. .|' || \n .||. .||. .||. ||. '|..'|'   .||.      '|..|' .||.    .|| || ||.  '|..|' |'..|' '|..'|' "
+    title_width = max(len(line) for line in title.split("\n")) - 1
+    title = ruler * title_width + "\n\n" + title + "\n\n" + ruler * (title_width - len(version_number) - 2) + " " + version_number + " " + ruler
+    print(title)
 
-
-title_print()
+title_print(" ")
