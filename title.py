b58488f2d1482f3b300ed588832612cbd6d26512
Hasnep <hasnep@gmail.com>
2018-09-18 11:33:37 +0100

    Change title to triple quoted string

--- title.py
+++ title.py
@@ -1,9 +1,15 @@
-version_number = "v0.1.157"
+version_number = "v0.1.158"
 
 
 def title_print(ruler: str="="):
-    title = " '||' '||  '||                '||''''| \n  ||   ||   || ..    ....      ||  .     ...   ... ..  .. .. ..     ...    ....   .... \n  ||   ||   ||' ||  '' .||     ||''|   .|  '|.  ||' ''  || || ||  .|  '|. ||. '  '' .|| \n  ||   ||   ||  ||  .|' ||     ||      ||   ||  ||      || || ||  ||   || . '|.. .|' || \n .||. .||. .||. ||. '|..'|'   .||.      '|..|' .||.    .|| || ||.  '|..|' |'..|' '|..'|' "
+    title = """
+'||' '||  '||                '||''''|
+ ||   ||   || ..    ....      ||  .     ...   ... ..  .. .. ..     ...    ....   ....
+ ||   ||   ||' ||  '' .||     ||''|   .|  '|.  ||' ''  || || ||  .|  '|. ||. '  '' .||
+ ||   ||   ||  ||  .|' ||     ||      ||   ||  ||      || || ||  ||   || . '|.. .|' ||
+.||. .||. .||. ||. '|..'|'   .||.      '|..|' .||.    .|| || ||.  '|..|' |'..|' '|..'|'
+"""
     title_width = max(len(line) for line in title.split("\n"))
-    title = ruler * title_width + "\n\n" + title + "\n\n" + ruler * (title_width - len(version_number) - 3) + " " + version_number + " " + ruler
+    title = ruler * title_width + "\n" + title + "\n" + ruler * (title_width - len(version_number) - 3) + " " + version_number + " " + ruler
     print(title)
     return
